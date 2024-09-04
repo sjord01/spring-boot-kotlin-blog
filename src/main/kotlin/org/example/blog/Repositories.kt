cbb20e7461048247e0760285e53467d0508c8c2c
Sam Ordonez <samsonordonez.ce@gmail.com>
2024-09-03 22:38:13 -0700

    updated application.properties

--- src/main/kotlin/org/example/blog/Repositories.kt
+++ src/main/kotlin/org/example/blog/Repositories.kt
@@ -2,7 +2,7 @@ package org.example.blog
 
 import org.springframework.data.repository.CrudRepository
 
-// these are Soring data JPA repos
+// these are Sorting data JPA repos
 
 interface ArticleRepository : CrudRepository<Article, Long> {
     fun findBySlug(slug: String): Article?
