fdb96f2dbee0d80a0f8b7e62977f1c40e7a78e2a
Sam Ordonez <samsonordonez.ce@gmail.com>
2024-09-03 22:56:55 -0700

    Reapply "updated application.properties"
    This reverts commit 2ab81c7e463fdd0440d9d56dd4cf6c31210b6292.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("plugin.jpa") version "1.9.25"
+    kotlin("kapt") version "1.9.22"
 }
 
 allOpen {
@@ -45,6 +46,14 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "mockito-core")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 kotlin {
@@ -62,3 +71,13 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs += "-Xjsr305=strict"
     }
 }
+
+/*
+For tests, instead of integration tests, we are going to leverage @WebMvcTest and Mockk
+which is similar to Mockito but better suited for Kotlin.
+Since @MockBean and @SpyBean annotations are specific to Mockito,
+we are going to leverage SpringMockK which provides similar @MockkBean and @SpykBean
+annotations for Mockk.
+ */
+
+
--- src/main/kotlin/org/example/blog/BlogApplication.kt
+++ src/main/kotlin/org/example/blog/BlogApplication.kt
@@ -2,10 +2,13 @@ package org.example.blog
 
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
 class BlogApplication
+// We need @EnableConfigurationProperties(BlogProperties::class) to support BlogProperties.kt's @ConfigurationProperties
 
 fun main(args: Array<String>) {
     runApplication<BlogApplication>(*args) {
